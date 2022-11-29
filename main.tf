c3addf28b93b05790eabf7bac2c47d58f552d0d6
anbhiman <40796080+anbhiman@users.noreply.github.com>
2022-11-29 15:11:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,11 +12,11 @@ provider "aws" {
     region = var.aws_region
 
 }
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-testtredev-bucket"
-  versioning {
-      enabled = true
-   }
-}
+#resource "aws_s3_bucket" "b" {
+ # bucket = "my-tf-testtredev-bucket"
+  #versioning {
+   #   enabled = true
+   #}
+#}
 
 
