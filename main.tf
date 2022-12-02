7886c39f1a8437300980251a8174a7a568d296e9
anbhiman <40796080+anbhiman@users.noreply.github.com>
2022-12-02 21:22:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,15 @@ resource "aws_athena_database" "myathenadb" {
   name   = "athena_database_2122022"
   bucket = aws_s3_bucket.mybucket.bucket
 }
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
