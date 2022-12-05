69364b8ee2d3b187568550f78f8a7562476c7751
anbhiman <40796080+anbhiman@users.noreply.github.com>
2022-12-05 20:40:40 +0530

    Create networking.tf

---
+++ networking.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "main" {
+ cidr_block = "10.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
+}
+
+resource "aws_subnet" "private_subnets" {
+ count      = length(var.private_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ cidr_block = element(var.private_subnet_cidrs, count.index)
+ 
+ tags = {
+   Name = "Private Subnet ${count.index + 1}"
+ }
+}
