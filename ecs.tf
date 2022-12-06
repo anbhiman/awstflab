7b6c4f052e7f3718bbecc34465cc070af21ac0a9
anbhiman <40796080+anbhiman@users.noreply.github.com>
2022-12-06 16:02:49 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -13,3 +13,12 @@ resource "aws_ecr_repository" "aws-ecr" {
     Environment = var.app_environment
   }
 }
+
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "${var.app_name}-${var.app_environment}-logs"
+
+  tags = {
+    Application = var.app_name
+    Environment = var.app_environment
+  }
+}
