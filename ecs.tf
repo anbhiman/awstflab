81c1385b5d766c7377b409295f48234811332efd
anbhiman <40796080+anbhiman@users.noreply.github.com>
2022-12-06 17:36:50 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -22,3 +22,28 @@ resource "aws_cloudwatch_log_group" "log-group" {
     Environment = var.app_environment
   }
 }
+
+resource "aws_ecs_task_definition" "test" {
+  family                   = "${var.app_name}-task"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "iis",
+    "image": "mcr.microsoft.com/windows/servercore/iis",
+    "cpu": 1024,
+    "memory": 2048,
+    "essential": true
+  }
+]
+TASK_DEFINITION
+
+  runtime_platform {
+    operating_system_family = "WINDOWS_SERVER_2019_CORE"
+    cpu_architecture        = "X86_64"
+  }
+  
+}
